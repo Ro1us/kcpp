d713fb161f18963058a00c00255a84c523edd053
Ro1us <b2rak@onet.pl>
2026-06-09 15:09:30 +0200

    Zadanie 1 fix

--- kcppBasic/src/ZadStudent/Zad_1/ZadCharExample.cc
+++ kcppBasic/src/ZadStudent/Zad_1/ZadCharExample.cc
@@ -1,21 +1,21 @@
-// #include <iostream>
-// #include <cstring>
-//
-// int main() {
-//     char napis1[] = "Hello";
-//     char napis2[20];
-//     strcpy(napis2, napis1);
-//     std::cout << "Po strcpy: "
-//               << napis2
-//               << std::endl;
-//     char napis3[20];
-//     strncpy(napis3, napis2, 3);
-//     napis3[3] = '\0';
-//     std::cout << "Po strncpy: "
-//               << napis3
-//               << std::endl;
-//     std::cout << "strlen: "
-//               << strlen(napis2)
-//               << std::endl;
-//     return 0;
-// }
+#include <iostream>
+#include <cstring>
+
+int main() {
+    char napis1[] = "Hello";
+    char napis2[20];
+    strcpy(napis2, napis1);
+    std::cout << "Po strcpy: "
+              << napis2
+              << std::endl;
+    char napis3[20];
+    strncpy(napis3, napis2, 3);
+    napis3[3] = '\0';
+    std::cout << "Po strncpy: "
+              << napis3
+              << std::endl;
+    std::cout << "strlen: "
+              << strlen(napis2)
+              << std::endl;
+    return 0;
+}
